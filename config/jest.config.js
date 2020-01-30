793003c25f708fed2bf53ce8cd1359fa017ce0a4
Caralyn Reisle <creisle@bcgsc.ca>
2020-01-30 14:58:42 -0800

    Add jest config

---
+++ config/jest.config.js
@@ -0,0 +1,41 @@
+// main jest configuration file
+const path = require('path');
+
+const BASE_DIR = path.resolve(__dirname, '..');
+
+module.exports = {
+    rootDir: BASE_DIR,
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: [
+        'src/**.js',
+        'src/**/*.js',
+        'src/**/**/*.js',
+    ],
+    coverageReporters: [
+        'clover',
+        'text',
+        'json',
+        'json-summary',
+        'lcov',
+    ],
+    reporters: [
+        'default',
+        [
+            'jest-junit',
+            {
+                outputDirectory: '<rootDir>/coverage/junit.xml',
+            },
+        ],
+    ],
+    testRunner: 'jest-circus/runner',
+    testRegex: 'test/.*\\.js',
+    testEnvironment: 'node',
+    testPathIgnorePatterns: [
+        '/node_modules/',
+    ],
+    moduleFileExtensions: [
+        'js',
+        'json',
+    ],
+};
